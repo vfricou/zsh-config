c4c7622ba5f1f026018225077bc8b4e38eeb4ada
root <root@nm20100.nimes.fr>
2015-08-06 17:43:11 +0200

    Modification to battery.sh with check if AC is engaged or not to display battery level or not

--- battery.sh
+++ battery.sh
@@ -1,7 +1,16 @@
 #!/bin/zsh
 # BATDIR is the folder with your battery characteristics
 BATDIR="/sys/class/power_supply/BAT0"
+ACSTAT=$(cat /sys/class/power_supply/AC/online)
+
 max=`cat $BATDIR/charge_full`
 current=`cat $BATDIR/charge_now`
 percent=$(( 100 * $current / $max ))
-echo "($percent%%)"
+
+if [ -n ${ACSTAT} ]
+then
+	if [ ${ACSTAT} != "1" ]
+	then
+		echo "($percent%%)"
+	fi
+fi
