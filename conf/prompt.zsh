4ca351e6c302df814d713c7f73adc5ab37aaaf9e
Vincent FRICOU <vincent+github@fricouv.eu>
2018-11-01 19:51:32 +0100

    fix: Bad prompt functionment. Revert on previous configuration

--- conf/prompt.zsh
+++ conf/prompt.zsh
@@ -1,35 +1,13 @@
-
-get_space () {
-  local STR=$1$2
-  local zero='%([BSUbfksu]|([FB]|){*})'
-  local LENGTH=${#${(S%%)STR//$~zero/}}
-  local SPACES=""
-  (( LENGTH = ${COLUMNS} - $LENGTH - 1))
-
-  for i in {0..$LENGTH}
-    do
-      SPACES="$SPACES "
-    done
-
-  echo $SPACES
-}
-
-if [[ $EUID -eq 0 ]]
+if [ $EUID -eq 0 ]
 then
-	_PLEFT="%{$fg[red]%}%~%{$reset_color%} ${GIT_PS1}"
-	_PRIGHT="%{$fg[red]%}%(?..%?)%{$reset_color%} [%m:%{$terminfo[bold]$fg[green]%}%n%{$reset_color%}] %T ${batteryp}"
-else
-	_PLEFT="%{%{$terminfo[bold]$fg[blue]%}%~%{$reset_color%} ${GIT_PS1}"
-	_PRIGHT="%{%{$terminfo[bold]$fg[blue]%}%(?..%?)%{$reset_color%} [%m:%{$terminfo[bold]$fg[green]%}%n%{$reset_color%}] %T ${batteryp}"
+       PROMPT="
+%{$fg[red]%}%~%{$reset_color%} ${GIT_PS1}
+%# "
+       RPROMPT="%{$fg[red]%}%(?..%?)%{$reset_color%} [%m:%{$terminfo[bold]$fg[red]%}%n%{$reset_color%}] %T ${batteryp}"
+       else
+       PROMPT="
+%{$terminfo[bold]$fg[blue]%}%~%{$reset_color%} ${GIT_PS1}
+%# "
+       RPROMPT="%{$fg[red]%}%(?..%?)%{$reset_color%} [%m:%{$terminfo[bold]$fg[green]%}%n%{$reset_color%}] %T ${batteryp}"
 fi
 
-prompt_precmd () {
-  _SPACES=`get_space $_PLEFT $_PRIGHT`
-  print
-  print -rP "$_PLEFT$_SPACES$_PRIGHT"
-}
-
-setopt prompt_subst
-PROMPT="%# "
-autoload -U add-zsh-hook
-add-zsh-hook precmd prompt_precmd
