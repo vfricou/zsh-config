252f4c66f1b2b3137751af9f123d1fe228e48762
Vincent FRICOU <vincent+github@fricouv.eu>
2018-11-18 14:14:26 +0100

    enc: remove line breaking in prompt

--- conf/prompt.zsh
+++ conf/prompt.zsh
@@ -1,12 +1,10 @@
 if [ $EUID -eq 0 ]
 then
-       PROMPT="
-%{$fg[red]%}%~%{$reset_color%} ${GIT_PS1}
+       PROMPT="%{$fg[red]%}%~%{$reset_color%} ${GIT_PS1}
 %# "
        RPROMPT="%{$fg[red]%}%(?..%?)%{$reset_color%} [%m:%{$terminfo[bold]$fg[red]%}%n%{$reset_color%}] %T ${batteryp}"
        else
-       PROMPT="
-%{$terminfo[bold]$fg[blue]%}%~%{$reset_color%} ${GIT_PS1}
+       PROMPT="%{$terminfo[bold]$fg[blue]%}%~%{$reset_color%} ${GIT_PS1}
 %# "
        RPROMPT="%{$fg[red]%}%(?..%?)%{$reset_color%} [%m:%{$terminfo[bold]$fg[green]%}%n%{$reset_color%}] %T ${batteryp}"
 fi
